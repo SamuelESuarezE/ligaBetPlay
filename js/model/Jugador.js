91738c8c3daaa533e52eaad824c2208656e9cc32
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com.>
2024-07-20 11:06:55 -0500

    feat: :construction: Progress in validation and aggregation of a player in the implementation of case 2 of LigaBetPlay
    The necessary validations of a player were carried out, such as his name and jersey number, so that when creating a new one, if it already exists, he will not be allowed to create it.

--- js/model/Jugador.js
+++ js/model/Jugador.js
@@ -1,5 +1,5 @@
 import { Connect } from "../../helpers/db/Connect.js";
-
+import { ObjectId } from "mongodb"; // agregamos ObjectId para poder validar,actualizar, eliminar, agregar por id de jugador
 
 export class Jugador extends Connect {
   static instanceJugador;
@@ -22,6 +22,27 @@ export class Jugador extends Connect {
     const data = await this.collection.find().toArray();
     await this.conexion.close();
     return data;
+  }
+  //caso de uso 2. Gestión de Jugadores
+ //Actor: Administrador de la Liga, Equipo Técnico
+
+  // funcion para agregar o crear un nuevo jugador en la base de datos
+  async addPlayer(player) {
+    await this.conexion.connect();
+    // creamos esta funcion donde verificamos que el jugador exista mediante la funcion de buscar(findone) y verificamos que existan 2 atributos escenciales para el jugador
+    const existingPlayer = await this.collection.findOne({
+      nombre: player.nombre,
+      numero_camiseta: player.numero_camiseta,
+    });
+    // si ya existe el jugador con el mismo nombre y camisa no dejara crearlo
+    if (existingPlayer) {
+      await this.conexion.close();
+      throw new Error("El jugador ya existe en el equipo.");
+    }
+    // si no existe realizamos la correcta agregacion del nuevo jugador
+    const result = await this.collection.insertOne(player);
+    await this.conexion.close();
+    return result;
   }
 
 }
