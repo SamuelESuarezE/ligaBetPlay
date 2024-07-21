ca10fd61d1a9fe12bb74780bfd07c57cb6a1d8fa
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com.>
2024-07-21 11:51:56 -0500

    feat: :construction: progress of the validation of the existence of a match in order to update its results in case 4 of LigaBetPlay
    We verify that the party ID exists so that it allows us to change its results because if it does not exist it will not let us update anything

--- js/model/Partido.js
+++ js/model/Partido.js
@@ -1,4 +1,5 @@
 import { Connect } from "../../helpers/db/Connect.js";
+import { ObjectId } from "mongodb";
 
 
 export class Partido extends Connect {
@@ -23,5 +24,23 @@ export class Partido extends Connect {
     await this.conexion.close();
     return data;
   }
+  // funcion para poder actualizar los resultados de un partido en tiempo real:
+  async addMatchResult(matchId, result) {
+    await this.conexion.connect();
+    // realizamos la verificacion de que el id del partido exista, si no existe no permitira actualizar nada
+    const objectId = new ObjectId(matchId.$oid);
+    const existingMatch = await this.collection.findOne({ _id: objectId });
 
+    if (!existingMatch) {
+      await this.conexion.close();
+      throw new Error("El partido no existe.");
+    }
+    // si existe el partido procedemos mediante updateone y $set a actualizar el resultado anterior por el reciente
+    const resultUpdate = await this.collection.updateOne(
+      { _id: objectId },
+      { $set: { resultado: result } }
+    );
+    await this.conexion.close();
+    return resultUpdate;
+  }
 }
