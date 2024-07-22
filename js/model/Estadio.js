c65cfa7373dcceb52a87983c7b6b516a0a5395a6
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-07-22 06:22:36 -0500

    feat: :sparkles: Method for getting all stadiums completed

--- js/model/Estadio.js
+++ js/model/Estadio.js
@@ -1,27 +1,34 @@
 import { Connect } from "../../helpers/db/Connect.js";
 
-
 export class Estadio extends Connect {
-  static instanceEstadio;
-  db;
-  collection;
+    static instanceEstadio;
+    db;
+    collection;
 
-  constructor() {
-    super();
-    this.db = this.conexion.db(this.getDbName);
-    this.collection = this.db.collection('estadio');
-    if (Estadio.instanceEstadio) {
-      return Estadio.instanceEstadio;
+    constructor() {
+        super();
+        this.db = this.conexion.db(this.getDbName);
+        this.collection = this.db.collection("estadio");
+        if (Estadio.instanceEstadio) {
+            return Estadio.instanceEstadio;
+        }
+        Estadio.instanceEstadio = this;
+        return this;
     }
-    Estadio.instanceEstadio = this;
-    return this;
-  }
 
-  async getAllStadiums() {
-    await this.conexion.connect();
-    const data = await this.collection.find().toArray();
-    await this.conexion.close();
-    return data;
-  }
-
+    async getAllStadiums() {
+        try {
+            await this.conexion.connect();
+            const data = await this.collection.find().toArray();
+            return data
+        } catch (error) {
+            return {
+                success: false,
+                error_type: error.name || "Error",
+                error_message: error.message || "Ha ocurrido un error desconocido",
+            };
+        } finally {
+            await this.conexion.close();
+        }
+    }
 }
